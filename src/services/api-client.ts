a5762134ea601d3f09452ba7e329bfaff2d78f35
Charaf Dev <charafeddine.dev@gmail.com>
2025-11-24 23:52:03 +0100

    fix: Proxy PHP file fix for the API Network Error issue on Prod

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,9 +2,12 @@ import axios from 'axios';
 
 // export default axios.create({
 //   baseURL: '/api', // 👈 this uses the Vite proxy
+// });
 
+// export default axios.create({
+//   baseURL: 'https://www.freetogame.com/api', // Direct production API
 // });
 
 export default axios.create({
-  baseURL: 'https://www.freetogame.com/api', // Direct production API
+  baseURL: 'https://f2p.charaf.dev/proxy.php?endpoint=', // Proxy PHP file fix for the API Network Error issue
 });
